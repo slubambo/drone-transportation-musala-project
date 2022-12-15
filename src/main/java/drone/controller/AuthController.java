7bcc6736241f40bbc20506b4af9512e6b056d74c
slubambo <slubambo57@gmail.com>
2022-12-15 11:49:37 +0300

    [feature] Auth Contoller
    Logging in and Signing up

---
+++ src/main/java/drone/controller/AuthController.java
@@ -0,0 +1,113 @@
+package drone.controller;
+
+import drone.Payloads.ApiResponse;
+import drone.Payloads.JwtAuthenticationResponse;
+import drone.Payloads.LoginRequest;
+import drone.Payloads.SignUpRequest;
+import drone.enums.RoleName;
+import drone.exception.AppException;
+import drone.model.General.Role;
+import drone.model.General.User;
+import drone.repository.General.RoleRepository;
+import drone.repository.General.UserRepository;
+import drone.security.JwtTokenProvider;
+import jakarta.validation.Valid;
+import java.net.URI;
+import java.util.Collections;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+  @Autowired
+  AuthenticationManager authenticationManager;
+
+  @Autowired
+  UserRepository userRepository;
+
+  @Autowired
+  RoleRepository roleRepository;
+
+  @Autowired
+  PasswordEncoder passwordEncoder;
+
+  @Autowired
+  JwtTokenProvider tokenProvider;
+
+  @PostMapping("/sign-in")
+  public ResponseEntity<?> authenticateUser(
+    @Valid @RequestBody LoginRequest loginRequest
+  ) {
+    Authentication authentication = authenticationManager.authenticate(
+      new UsernamePasswordAuthenticationToken(
+        loginRequest.getUsernameOrEmail(),
+        loginRequest.getPassword()
+      )
+    );
+
+    SecurityContextHolder.getContext().setAuthentication(authentication);
+
+    String jwt = tokenProvider.generateToken(authentication);
+    return ResponseEntity.ok(new JwtAuthenticationResponse(jwt));
+  }
+
+  @PostMapping("/signup")
+  public ResponseEntity<?> registerUser(
+    @Valid @RequestBody SignUpRequest signUpRequest
+  ) {
+    if (userRepository.existsByUsername(signUpRequest.getUsername())) {
+      return new ResponseEntity(
+        new ApiResponse(false, "Username is already taken!"),
+        HttpStatus.BAD_REQUEST
+      );
+    }
+
+    if (userRepository.existsByEmail(signUpRequest.getEmail())) {
+      return new ResponseEntity(
+        new ApiResponse(false, "Email Address already in use!"),
+        HttpStatus.BAD_REQUEST
+      );
+    }
+
+    // Creating user's account
+    User user = new User(
+      signUpRequest.getName(),
+      signUpRequest.getUsername(),
+      signUpRequest.getEmail(),
+      signUpRequest.getPassword()
+    );
+
+    user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+    Role userRole = roleRepository
+      .findByName(RoleName.ROLE_USER)
+      .orElseThrow(() -> new AppException("User Role not set."));
+
+    user.setRoles(Collections.singleton(userRole));
+
+    User result = userRepository.save(user);
+
+    URI location = ServletUriComponentsBuilder
+      .fromCurrentContextPath()
+      .path("/api/users/{username}")
+      .buildAndExpand(result.getUsername())
+      .toUri();
+
+    return ResponseEntity
+      .created(location)
+      .body(new ApiResponse(true, "User registered successfully"));
+  }
+}
