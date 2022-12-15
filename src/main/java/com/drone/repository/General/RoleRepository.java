b594ac4a0bd62065373e571a4ad0b69d1865a950
slubambo <slubambo57@gmail.com>
2022-12-15 09:48:45 +0300

    [feature] setting up repositories

---
+++ src/main/java/com/drone/repository/General/RoleRepository.java
@@ -0,0 +1,12 @@
+package com.drone.repository.General;
+
+import com.drone.enums.RoleName;
+import com.drone.model.General.Role;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+  Optional<Role> findByName(RoleName roleName);
+}
